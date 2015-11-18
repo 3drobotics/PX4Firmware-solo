7bd570a8033f912031601eb4cb16b9e3d3639bb9
David Sidrane <david_s5@nscdg.com>
2015-11-18 04:50:53 -1000

    Renamed pax4fmu-v3 to pax4fmu-v4

--- src/drivers/meas_airspeed/meas_airspeed.cpp
+++ src/drivers/meas_airspeed/meas_airspeed.cpp
@@ -331,7 +331,8 @@ MEASAirspeed::cycle()
 void
 MEASAirspeed::voltage_correction(float &diff_press_pa, float &temperature)
 {
-#if defined(CONFIG_ARCH_BOARD_PX4FMU_V2) || defined(CONFIG_ARCH_BOARD_PX4FMU_V3)
+#if defined(CONFIG_ARCH_BOARD_PX4FMU_V2) || defined(CONFIG_ARCH_BOARD_PX4FMU_V4)
+
 	if (_t_system_power == -1) {
 		_t_system_power = orb_subscribe(ORB_ID(system_power));
 	}
@@ -374,7 +375,7 @@ MEASAirspeed::voltage_correction(float &diff_press_pa, float &temperature)
 		voltage_diff = -1.0f;
 	}
 	temperature -= voltage_diff * temp_slope;
-#endif // defined(CONFIG_ARCH_BOARD_PX4FMU_V2) || defined(CONFIG_ARCH_BOARD_PX4FMU_V3)
+#endif // defined(CONFIG_ARCH_BOARD_PX4FMU_V2) || defined(CONFIG_ARCH_BOARD_PX4FMU_V4)
 }
 
 /**
