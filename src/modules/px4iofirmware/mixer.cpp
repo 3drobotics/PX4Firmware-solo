4086286a4230259ffc08da28934164e0ed4fd8c0
Andrew Tridgell <andrew@tridgell.net>
2016-01-22 17:25:05 +1100

    px4iofirmware: fixed a bug with override handling
    this fixes a race condition between the DMA completion handler
    updating registers in px4io and the mixer used for handling the
    override state. The register set code could set r_page_servos[]
    between the time when pwm_limit_calc() is called and the servos are
    actually output.

--- src/modules/px4iofirmware/mixer.cpp
+++ src/modules/px4iofirmware/mixer.cpp
@@ -135,8 +135,9 @@ mixer_tick(void)
 			 * triggered, with FMU active */
 			source = MIX_OVERRIDE_FMU_OK;
 		} else {
-			/* don't actually mix anything - we already have raw PWM values */
+			/* don't actually mix anything - copy values from r_page_direct_pwm */
 			source = MIX_NONE;
+			memcpy(r_page_servos, r_page_direct_pwm, sizeof(uint16_t)*PX4IO_SERVO_COUNT);
 		}
 	} else {
 
